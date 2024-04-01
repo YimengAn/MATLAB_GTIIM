b015733666768fea34a4a2828dd133efdcb42e91
YimengAn <103626184+YimengAn@users.noreply.github.com>
2024-04-01 17:46:26 +0100

    Dynamic_Time_Warping.m
    Make sure to define the 'GetMin' and 'GetMinIndex' functions for your code to run successfully.

--- Dynamic_Time_Warping.m
+++ Dynamic_Time_Warping.m
@@ -1,38 +1,44 @@
-%  程序名称: Dynamic Time Warping
-%  数据输入：各数组序列
-%  结果输出：DTW距离
-clc,clear all;
+% Program Name: Dynamic Time Warping model (DTW)
+% Data Input: Time series sequences
+% Result Output: DTW Distance
+
+clc;
+clear all;
 format short g;
-fprintf(2,'Dynamic Time Warping distance：\n')
-
-%% %%%%%%%%%%%%%%%%%%%%%%%% 将.xls数据文件添加到路径 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fprintf(2,'Dynamic Time Warping distance:\n')
 
 
-%% %%%%%%%%%%%%%%%%%%%%%%%% 计算DTW distance %%%%%%%%%%%%%%%%%%%%%%
-% 数据输入（第一行为参照序列）
-sheetNames = sheetnames("AQI.xls");
-data = readtable('AQI.xls',"ReadRowNames",false,"ReadVariableNames",false,"Sheet",sheetNames{28},"Range","C2:Z29"); % 读入table
-data = data{:,:}; % table 转 matrix
-[m,n] = size(data); % m=m1+1，n=n1
+%% Data Input (the end row is the reference sequence)
+sheetNames = sheetnames("Pollution.xlsx");
+pollutants = size(sheetNames,1);
+% Select research indicators (e.g. sheetNames{1} = AQI)
+Pollution = readtable('Pollution.xlsx',"ReadRowNames",false,"ReadVariableNames",false,...
+    "Sheet",sheetNames{1},"Range","B2:Y29"); % Read as table
 
+refer = Pollution{end,:};
+data = Pollution{1:end-1,:};
+[object,time] = size(data); 
+
+%% Calculate DTW distance
 DTW_degree=[];
-object = 27;
 for z = 1:object
-    %生成两个有明显平移性质的时间序列
-    x = data(1,:);
-    y = data(z+1,:);
+    % Generate two time series with significant translation properties
+    x = refer;
+    y = data(z,:);
     x_len = length(x);
     y_len = length(y);
-%     plot(1:x_len,x);hold on
-%     plot(1:y_len,y);hold on
-    %计算两序列每个特征点的距离矩阵
+    plot(1:x_len,x); hold on
+    plot(1:y_len,y); hold on
+    
+    % Compute the distance matrix for each feature point of the two sequences
     distance = zeros(x_len,y_len);
     for i = 1:x_len
         for j=1:y_len
             distance(i,j) = (x(i)-y(j)).^2;
         end
     end
-    %计算两个序列
+    
+    % Calculate the cumulative distance matrix
     DP = zeros(x_len,y_len);
     DP(1,1) = distance(1,1);
     for i=2:x_len
@@ -46,11 +52,13 @@ for z = 1:object
             DP(i,j) = distance(i,j) + GetMin(DP(i-1,j),DP(i,j-1),DP(i-1,j-1));
         end
     end
-    %回溯，找到各个特征点之间的匹配关系
+    
+    % Backtrack to find the matching relationship between each feature point
     i = x_len;
     j = y_len;
     while(~((i == 1)&&(j==1)))
-        plot([i,j],[x(i),y(j)],'b');hold on %画出匹配之后的特征点之间的匹配关系
+        plot([i,j],[x(i),y(j)],'b');hold on 
+            % Plot the matching relationship between feature points
         if(i==1)
             index_i = 1;
             index_j = j-1;
@@ -63,10 +71,10 @@ for z = 1:object
         i = index_i;
         j = index_j;  
     end
-    DTW_degree = [DTW_degree,DP(1,24)];
+    DTW_degree = [DTW_degree,DP(1,time)];
 end
 
-% 结果输出
+% Result Output
 disp('DTW_distance:');
 disp(DTW_degree);
 
